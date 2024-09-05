ed31a33e96e24aa79b85e78a431807de80c02742
bhumikvaru <91276141+bhumikvaru@users.noreply.github.com>
2024-09-05 15:31:30 +0530

    Time: 41 ms (97.48%) | Memory: 48.5 MB (93.04%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,24 +4,15 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-    let uniqueCount = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== val) uniqueCount += 1;
+let c = 0;
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val)
+    {
+        nums[c] = nums[i];
+        c++;
     }
-
-    let backpoint = nums.length - 1;
-
-    for (let i = 0; i < uniqueCount; i++) {
-        if (nums[i] === val) {
-            while (nums[backpoint] === val) {
-                backpoint = backpoint - 1;
-            }
-            console.log(nums[backpoint]);
-            nums[i] = nums[backpoint];
-            backpoint = backpoint - 1;
-        }
-    }
-    return uniqueCount;
+}
+return c;
 };
 
 
