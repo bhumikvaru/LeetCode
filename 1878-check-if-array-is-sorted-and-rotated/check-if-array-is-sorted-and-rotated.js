6089a7ebc869340b4a2d7319ef0fef937056deb8
bhumikvaru <91276141+bhumikvaru@users.noreply.github.com>
2024-09-26 12:51:43 +0530

    Time: 53 ms (52.98%) | Memory: 48.4 MB (91.23%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
@@ -4,8 +4,10 @@
  */
 var check = function (nums) {
     let count = 0;
+    //only one element will always be sorted
     if (nums.length === 1)
         return true;
+    // if the order sequence is broken more than once its not rotated
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] > (nums[(i + 1) % nums.length])) {
             count++;
