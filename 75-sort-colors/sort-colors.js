d7ab049467ec1bac193aae687815153e98bf8e86
bhumikvaru <91276141+bhumikvaru@users.noreply.github.com>
2024-10-01 17:56:27 +0530

    Time: 59 ms (26.60%) | Memory: 49.7 MB (13.39%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -2,16 +2,25 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var sortColors = function(nums) {
-   for (let i = 0; i < nums.length - 1; i++) {
-        // Inner loop for each pair comparison
-        for (let j = 0; j < nums.length- i - 1; j++) {
-            // Swap if the element is greater than the next
-            if (nums[j] > nums[j + 1]) {
-                let temp = nums[j];
-                nums[j] = nums[j + 1];
-                nums[j + 1] = temp;
-            }
+var sortColors = function (nums) {
+    let hashMap = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (hashMap[nums[i]])
+            hashMap[nums[i]] = hashMap[nums[i]] + 1;
+        else
+            hashMap[nums[i]] = 1
+    };
+    let index = 0;
+    let count = 0;
+    for (set in hashMap) {
+        while (count<hashMap[set])
+        {
+            nums[index] = parseInt(set);
+            index++;
+            count++;
         }
-   }
-        };
+        count = 0;
+    }
+
+    console.log(hashMap)
+};
