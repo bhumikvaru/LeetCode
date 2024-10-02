a25d0c75c6536facf280f6525317344eb948a39e
bhumikvaru <91276141+bhumikvaru@users.noreply.github.com>
2024-10-02 22:34:07 +0530

    Time: 80 ms (19.73%) | Memory: 58.9 MB (16.15%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -3,14 +3,11 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-    let localsum = 0;
+    let localsum = nums[0];
     let maxsum = nums[0];
-    for (let i = 0; i < nums.length; i++) {
-        console.log("nums[i]", nums[i])
-        localsum+=nums[i];
-        if(localsum>maxsum) maxsum = localsum;
-        if(localsum<0) localsum=0;
-        console.log("maxsum", maxsum)
+    for (let i = 1; i < nums.length; i++) {
+        localsum = Math.max(nums[i],localsum+nums[i])
+        maxsum = Math.max(localsum,maxsum)
     }
     return maxsum
 };
