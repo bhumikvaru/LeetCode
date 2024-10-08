733bd258240aaba190e8b7e53c36f23d94b219cd
Bhumik Varu <91276141+bhumikvaru@users.noreply.github.com>
2024-10-08 22:44:15 +0530

    Time: 915 ms (40.61%) | Memory: 75.4 MB (18.06%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -2,29 +2,28 @@
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function(nums) {
-    if(nums.length === 0) return 0;
-    if(nums.length === 1) return 1;
-    nums= nums.sort((a,b) => a-b);
+var longestConsecutive = function (nums) {
+    if (nums.length === 0) return 0;
+    if (nums.length === 1) return 1;
+    const numsSet = new Set(nums);
     let maxlength = 0;
     let count = 1;
-    console.log(nums)
-    for(let i=1;i<nums.length;i++)
-    {
-        if(nums[i] === (nums[i-1]+1))
-        {
-            console.log("nums[i]",nums[i])
-            console.log("count updated")
-            count++;
-        }
-        else if(nums[i] === nums[i-1]){}
-        else{
-            console.log("broken here", nums[i])
-            maxlength = Math.max(maxlength,count)
-            count =1;
-        }
+    for (let num of numsSet) {
+        console.log(num)
+        if (!numsSet.has(num - 1)) {
+            count = 1;
+            let current = num;
+            while(numsSet.has(current+1))
+            {
+                count++;
+                current = current+1;
+            }
+            maxlength = Math.max(count, maxlength);
     }
-    if(count>0)  maxlength = Math.max(maxlength,count)
+    }
+    if (count > 0)
+        maxlength = Math.max(count, maxlength);
 
+    // console.log(maxlength)
     return maxlength;
 };
