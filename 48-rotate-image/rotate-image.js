340b5a44c793042dc4f2e87db5dbfb0f0fdef756
Bhumik Varu <91276141+bhumikvaru@users.noreply.github.com>
2024-10-11 14:17:02 +0530

    Time: 64 ms (10.13%) | Memory: 52 MB (5.82%) - LeetSync

--- 48-rotate-image/rotate-image.js
+++ 48-rotate-image/rotate-image.js
@@ -7,9 +7,7 @@ var rotate = function(matrix) {
     {
         for(let j =i+1;j<matrix[i].length;j++)
         {
-            let temp = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i]= temp;
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
         }
     }
     console.log(matrix)
