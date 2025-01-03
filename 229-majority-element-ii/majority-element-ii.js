7f8ba5567abc6362adc02e1d23dd106791cbfecd
Bhumik Varu <91276141+bhumikvaru@users.noreply.github.com>
2025-01-03 16:23:45 +0530

    Time: 808 ms (6.01%) | Memory: 59.2 MB (5.30%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.js
+++ 229-majority-element-ii/majority-element-ii.js
@@ -3,21 +3,43 @@
  * @return {number[]}
  */
 var majorityElement = function(nums) {
-    let map = new Map();
-    for(let i =0;i<nums.length;i++)
+    let count1 = 0;
+    let elem1 =0;
+    let count2=0;
+    let elem2 = 0;
+    for(let i=0;i<nums.length ;i++)
     {
-        if(map.has(nums[i]))
-            map.set(nums[i], map.get(nums[i]) + 1)    
-            else
-             map.set(nums[i], 1)
+        if(count1 ===0 && elem2 != nums[i])
+        {count1++; elem1 = nums[i]}
+        else if(count2 ===0 && elem1 != nums[i])
+        {count2++; elem2 = nums[i]}
+        else if(elem1 === nums[i])
+        count1++;
+        else if(elem2 === nums[i])
+        count2++;
+        else 
+            {
+                count1--; count2--;
+            }
+
+            console.log("nums", nums[i]);
+            console.log("element1", elem1);
+            console.log("count1", count1);
+            console.log("element2", elem2);
+            console.log("count2", count2);
     }
-    const result=[];
-    for(elem of map)
+    let result = [];
+    count1 =0, count2 =0;
+    for(let i=0;i<nums.length;i++)
     {
-        console.log(elem)
-        if (elem[1] > Math.floor(nums.length/3))
-        result.push(elem[0])
-        if(result.length ===2) break;
+        if(nums[i] === elem1)  count1++;
+        else if(nums[i] === elem2) count2++;
     }
+
+    if(count1 > Math.floor(nums.length/3))
+    result.push(elem1)
+    if(count2 > Math.floor(nums.length/3))
+    result.push(elem2)
+    console.log(result);
     return result;
 };
