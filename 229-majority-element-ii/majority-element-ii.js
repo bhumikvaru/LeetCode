df79f6e2b549ebcb8bf5b3a9f18050d977e01018
Bhumik Varu <91276141+bhumikvaru@users.noreply.github.com>
2025-01-03 16:09:38 +0530

    Time: 19 ms (6.01%) | Memory: 52.8 MB (24.73%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var majorityElement = function(nums) {
+    let map = new Map();
+    for(let i =0;i<nums.length;i++)
+    {
+        if(map.has(nums[i]))
+            map.set(nums[i], map.get(nums[i]) + 1)    
+            else
+             map.set(nums[i], 1)
+    }
+    const result=[];
+    for(elem of map)
+    {
+        console.log(elem)
+        if (elem[1] > Math.floor(nums.length/3))
+        result.push(elem[0])
+    }
+    return result;
+};
