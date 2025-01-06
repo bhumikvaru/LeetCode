5eb199479e7df465b4097ee498a7ce5dc514e97a
Bhumik Varu <91276141+bhumikvaru@users.noreply.github.com>
2025-01-06 13:00:55 +0530

    Time: 353 ms (7.83%) | Memory: 53.9 MB (36.09%) - LeetSync

---
+++ 18-4sum/4sum.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function (nums, target) {
+    const results = [];
+    nums.sort((a, b) => a - b); // Sort the array
+
+    for (let i = 0; i < nums.length - 3; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        for (let j = i + 1; j < nums.length - 2; j++) {
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue; // Skip duplicates for j
+
+            for (let k = j + 1; k < nums.length - 1; k++) {
+                if (k > j + 1 && nums[k] === nums[k - 1]) continue; // Skip duplicates for k
+
+
+                for (let l = k + 1; l < nums.length; l++) {
+                    if (l > k + 1 && nums[l] === nums[l - 1]) continue; // Skip duplicates for l
+
+                    if (nums[i] + nums[j] + nums[k] + nums[l] === target) {
+                        results.push([nums[i], nums[j], nums[k], nums[l]]);
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+    return results;
+};
